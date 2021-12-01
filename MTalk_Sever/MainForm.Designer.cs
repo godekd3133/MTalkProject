493ec963bb609a8fe524075e96d2adc5f77685b0
MinKyu Kim <godekd3133@nine-m.com>
2021-12-01 11:21:53 +0900

    1:1 서버 오픈

---
+++ MTalk_Sever/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace MTalk_Sever
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IPAddressText = new System.Windows.Forms.TextBox();
+            this.PortText = new System.Windows.Forms.TextBox();
+            this.ServerOpenButton = new System.Windows.Forms.Button();
+            this.ServerStatusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // IPAddressText
+            // 
+            this.IPAddressText.Location = new System.Drawing.Point(37, 57);
+            this.IPAddressText.Name = "IPAddressText";
+            this.IPAddressText.Size = new System.Drawing.Size(100, 21);
+            this.IPAddressText.TabIndex = 0;
+            // 
+            // PortText
+            // 
+            this.PortText.Location = new System.Drawing.Point(157, 57);
+            this.PortText.Name = "PortText";
+            this.PortText.Size = new System.Drawing.Size(100, 21);
+            this.PortText.TabIndex = 1;
+            // 
+            // ServerOpenButton
+            // 
+            this.ServerOpenButton.Location = new System.Drawing.Point(37, 103);
+            this.ServerOpenButton.Name = "ServerOpenButton";
+            this.ServerOpenButton.Size = new System.Drawing.Size(219, 59);
+            this.ServerOpenButton.TabIndex = 2;
+            this.ServerOpenButton.Text = "button1";
+            this.ServerOpenButton.UseVisualStyleBackColor = true;
+            this.ServerOpenButton.Click += new System.EventHandler(this.ServerOpenButton_Click);
+            // 
+            // ServerStatusLabel
+            // 
+            this.ServerStatusLabel.AutoSize = true;
+            this.ServerStatusLabel.Location = new System.Drawing.Point(71, 187);
+            this.ServerStatusLabel.Name = "ServerStatusLabel";
+            this.ServerStatusLabel.Size = new System.Drawing.Size(65, 12);
+            this.ServerStatusLabel.TabIndex = 3;
+            this.ServerStatusLabel.Text = "서버상태 : ";
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ServerStatusLabel);
+            this.Controls.Add(this.ServerOpenButton);
+            this.Controls.Add(this.PortText);
+            this.Controls.Add(this.IPAddressText);
+            this.Name = "MainForm";
+            this.Text = "MTalk";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox IPAddressText;
+        private System.Windows.Forms.TextBox PortText;
+        private System.Windows.Forms.Button ServerOpenButton;
+        private System.Windows.Forms.Label ServerStatusLabel;
+    }
+}
+
---
+++ MTalk_Sever/MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MTalk_Sever
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(Object sender, EventArgs e)
+        {
+        }
+
+        async static Task AysncEchoServer()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 7000);
+            listener.Start();
+            while (true)
+            {
+                // 비동기 Accept                
+                TcpClient tc = await listener.AcceptTcpClientAsync().ConfigureAwait(false);
+
+                // 새 쓰레드에서 처리
+                Task.Factory.StartNew(AsyncTcpProcess, tc);
+            }
+        }
+
+        async static void AsyncTcpProcess(object o)
+        {
+            TcpClient tc = (TcpClient)o;
+
+            int MAX_SIZE = 1024;  // 가정
+            NetworkStream stream = tc.GetStream();
+
+            // 비동기 수신            
+            var buff = new byte[MAX_SIZE];
+            var nbytes = await stream.ReadAsync(buff, 0, buff.Length).ConfigureAwait(false);
+           
+            if (nbytes > 0)
+            {
+                string msg = Encoding.ASCII.GetString(buff, 0, nbytes);
+                //Console.WriteLine($"{msg} at {DateTime.Now}");
+
+                // 비동기 송신
+                await stream.WriteAsync(buff, 0, nbytes).ConfigureAwait(false);
+            }
+
+            stream.Close();
+            tc.Close();
+        }
+
+        private void ServerOpenButton_Click(Object sender, EventArgs e)
+        {
+            AysncEchoServer();
+            ServerStatusLabel.Text = "서버 상태 : ON";
+        }
+
+
+    }
+}
